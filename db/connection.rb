3f6a32f51ec4ef9fd03f19379997c493a73bff0b
Adam Bray <adam.bray@yale.edu>
2015-07-22 15:25:59 -0400

    updated connection, added rack config

--- db/connection.rb
+++ db/connection.rb
@@ -1,8 +1,5 @@
 # Load Active Record and connect to the DB
-ActiveRecord::Base.establish_connection({
-  database: 'tunr_db',
-  adapter: 'postgresql'
-})
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/tunr_db')
 
 # Fix an issue with sinatra and Active Record where connections are left open
 after do
