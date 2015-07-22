72cbbda92b70cebcbffc10f70a6d1bd429eb4c64
Adam Bray <adam.bray@yale.edu>
2015-07-22 14:26:39 -0400

    created starter from solution

--- app.rb
+++ app.rb
@@ -3,22 +3,12 @@ require 'sinatra/reloader'
 require 'active_record'
 
 # Load the file to connect to the DB
-require_relative 'db/connection.rb'
 
 # Load specific routes / controllers
-require_relative 'controllers/artists'
-require_relative 'controllers/songs'
-require_relative 'controllers/playlists'
 
 # Load models
-require_relative 'models/artist'
-require_relative 'models/song'
-require_relative 'models/playlist'
+
 
 ####################
 #  General routes  #
 ####################
-
-get '/' do
-  erb(:home)
-end
--- models/song.rb
+++
@@ -1,4 +0,0 @@
-class Song < ActiveRecord::Base
-  belongs_to :artist
-  has_and_belongs_to_many :playlists
-end
