55a65bee99ef036edb17fba06946e0f2790a1598
Adam Bray <adam.bray@yale.edu>
2015-07-22 15:06:06 -0400

    added solution step 2

--- app.rb
+++ app.rb
@@ -3,12 +3,19 @@ require 'sinatra/reloader'
 require 'active_record'
 
 # Load the file to connect to the DB
+require_relative 'db/connection.rb'
 
 # Load specific routes / controllers
+# Omitted for now
 
 # Load models
-
+require_relative 'models/artist'
+require_relative 'models/song'
 
 ####################
 #  General routes  #
 ####################
+
+get "/" do
+  erb :home
+end
